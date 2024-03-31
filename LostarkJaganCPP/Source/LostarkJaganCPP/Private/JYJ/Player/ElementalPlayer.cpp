0887d37d9685e27226c3db4bf1f35f496458e99e
NIJJY7 <102662924+NIJJY7@users.noreply.github.com>
2024-03-31 23:38:31 +0900

    Topview import

---
+++ LostarkJaganCPP/Source/LostarkJaganCPP/Private/JYJ/Player/ElementalPlayer.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JYJ/Player/ElementalPlayer.h"
+
+// Sets default values
+AElementalPlayer::AElementalPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AElementalPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AElementalPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AElementalPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ LostarkJaganCPP/Source/LostarkJaganCPP/Private/JYJ/TopviewEx/TP_TopDownGameMode.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JYJ/TopviewEx/TP_TopDownGameMode.h"
+#include "JYJ/TopviewEx/TP_TopDownPlayerController.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATP_TopDownGameMode::ATP_TopDownGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ATP_TopDownPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
